b208d318c2b0bef1c6c43c927034ba870a73f5e1
Brienna Herold <brienna.herold@gmail.com>
2016-05-15 15:46:52 -0400

    Display five messages

---
+++ Chapter 1/WelcomeFiveTimes.java
@@ -0,0 +1,10 @@
+// Write a program that displays Welcome to Java! five times.
+
+public class WelcomeFiveTimes {
+	public static void main(String[] args) {
+		// Display message five times
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Welcome to Java!");
+		}
+	}
+}
