555e2b471c129b7a4442feaceaacca2b9a465b34
Brienna Herold <brienna.herold@gmail.com>
2016-05-15 16:37:05 -0400

    Display a table

---
+++ Chapter 1/SquaresCubesTable.java
@@ -0,0 +1,19 @@
+/* Write a program that displays the following table:
+
+a       a^2     a^3 
+1       1       1   
+2       4       8
+3       9       27 
+4       16      64
+*/ 
+
+public class SquaresCubesTable {
+    public static void main(String[] args) {
+        System.out.println("a\ta^2\ta^3");
+        for(int row = 0; row <= 4; row++) {
+            int squared = (int) Math.pow(row, 2);
+            int cubed = (int) Math.pow(row, 3);
+            System.out.println(row + "\t" + squared + "\t" + cubed);
+        }
+    }
+}
